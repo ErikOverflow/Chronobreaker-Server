6553b123cf6c4e519cb00f30c1e36fc9ca369fb2
Erik <ErikOverflow@gmail.com>
2020-08-11 15:47:59 -0500

    Move account and region parsing to middleware

--- api/league.js
+++ api/league.js
@@ -8,21 +8,34 @@ const wsconfig = {
     }
 }
 
-const getAccount = (req, res) => {
-    return res.status(200).json(req.account);
+//Middleware to add "lol" object to the request and get region of request. Region is require on all requests
+const zDrive = (req,res,next) => {
+    req.lol = {};
+    req.lol.region = req.query.region;
+    return next();
 }
 
-const parseAccount = (req,res,next) => {
-    const summonerName = req.query.summonerName;
-    const region = req.query.region;
-    const url = riotApi.summoner.byName(region, summonerName);
-    return axios.get(url, wsconfig).then(result => {
-        req.account = result.data;
+//Middleware to optionally parse the account and fetch it from Riot on any requests. the Quey Parameter "SummonerName" is on the request, it adds Riot's account details.
+const accountParser = (req,_,next) => {
+    if(req.query.summonerName && req.lol.region){
+        const summonerName = req.query.summonerName;
+        const url = riotApi.summoner.byName(req.lol.region, summonerName);
+        return axios.get(url, wsconfig).then(result => {
+            req.lol.account = result.data;
+            return next();
+        })
+    }
+    else{
         return next();
-    })
+    }
+}
+
+const getAccount = (req, res) => {
+    return res.status(200).json(req.lol.account);
 }
 
 module.exports = {
+    zDrive,
+    accountParser,
     getAccount,
-    parseAccount
 }
--- index.js
+++ index.js
@@ -6,14 +6,18 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+const league = require('./api/league');
+
+//Middleware for 
+app.use(league.zDrive);
+app.use(league.accountParser);
+
 var v1 = express.Router();
 v1.get("/ping", (req,res) => {
     res.status(200).json({ ping: "Server is running."});
 });
 
-const league = require('./api/league');
-
-v1.get("/account", league.parseAccount, league.getAccount);
+v1.get("/account", league.getAccount);
 
 app.use("/api/v1", v1);
 
