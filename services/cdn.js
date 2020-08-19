92619d353e0f14519f8610f01b4b9de2ddaa234c
Erik <ErikOverflow@gmail.com>
2020-08-19 12:18:23 -0500

    Autobuild CDN

---
+++ services/cdn.js
@@ -0,0 +1,49 @@
+const tar = require('tar');
+const axios = require("axios");
+const fs = require('fs');
+
+const versionList = 'http://ddragon.leagueoflegends.com/api/versions.json';
+//Location of the versioned tarball
+const tarSet = (version) => `https://ddragon.leagueoflegends.com/cdn/dragontail-${version}.tgz`
+const baseUri = 'C:/CDNData';
+const championImagePath = `${baseUri}/img/champion`;
+module.exports = {
+    championImagePath
+}
+const wsconfig = {
+    method: "get",
+    responseType: "stream"
+  };
+let latestFolder;
+
+//Gets the latest tar and stores it to local disk. This is ideal if the CDN is hosted on the same server as this script.
+const updateTar = async () => {
+    let versions;
+    try{
+        versions = (await axios.get(versionList)).data;
+    }
+    catch{
+        console.error("Unable to fetch updated version list");
+        return;
+    }
+    versionIndex = 0;
+    let tarballResponse, version;
+    do{
+        version = versions[versionIndex++];
+        if(fs.existsSync(`${baseUri}/${version}`)){
+            return baseUri;
+        }
+        try{
+            tarballResponse = await axios.get(tarSet(version), wsconfig);
+        }
+        catch{
+            console.error(`Unable to find tarball for version ${version}`);
+        }
+    } while (tarballResponse.status != 200)
+    //If you want to store your files on an S3 bucket or external CDN, then the response should be streamed to that destination and extracted there.
+    await tarballResponse.data.pipe(tar.x({C: baseUri, strip: 1}, [`${version}/img`]));
+    fs.writeFileSync(baseUri + '/' + version);
+    return baseUri;
+}
+
+updateTar();
